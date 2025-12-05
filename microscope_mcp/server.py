09c011638cceb348c5feef08e205ed9a7f96acbf
jenuc <jenu.chacko@wisc.edu>
2025-12-05 03:19:16 -0600

    how to use http ports if needed

--- microscope_mcp/server.py
+++ microscope_mcp/server.py
@@ -5,7 +5,7 @@ from microscope_api import Microscope
 scope = Microscope()
 
 # MCP app
-mcp = FastMCP("Microscope MCP Server")
+mcp = FastMCP("Microscope MCP")
 
 
 @mcp.tool()
@@ -27,4 +27,14 @@ def get_stage_position():
 
 
 if __name__ == "__main__":
+
+    # as STDIO
     mcp.run()
+
+    ## as network MCP server
+    # mcp.run(transport="streamable-http", host="127.0.0.1", port=4200, path="/mcp")
+    # or async version
+    # import asyncio
+    # async def main():
+    #     await mcp.run_streamable_http_async(host="127.0.0.1", port=4200)
+    # asyncio.run(main())
